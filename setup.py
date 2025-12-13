02da481c81e5136a6b74a63816647417a01fca22
davetej <davetej305@gmail.com>
2025-12-12 19:16:53 -0500

    Adding init files to make sure the files are inclueded in the package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_portal",
+    author="Tej Dave",
+    version="0.1",
+    description="An intelligent document analysis and comparison system powered by LLMs",
+    include_package_data=True,
+    python_requires=">=3.10",
+)
+
